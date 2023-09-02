2a5b7f628676fa25c88ae59945f0e85f301769fd
kusk <kuskbee@github.com>
2023-09-03 01:01:50 +0900

    디노런 첫 커밋
    골드메탈 유튜브 강좌 참고 모작 : https://youtu.be/iENDSs0qXSs?si=EEGhv9w4kvM-azv9

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float Level(int score)
+    {
+        // 점수를 100으로 나누어 속도가 추가되는 로직 작성
+        int defaultSpeed = -5;
+        int increaseSpeed = (score * -1) / 100;
+
+        if(increaseSpeed < -5) {
+            increaseSpeed = -5;
+        }
+
+        return defaultSpeed + increaseSpeed;
+    }
+}
